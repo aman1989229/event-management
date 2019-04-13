fb517ec939e02d9dc7c09afa0233d01d19a9d4bf
Pawan <pk88267@gmail.com>
2019-04-13 19:34:18 +0530

    pawan contact page and its files

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -4,8 +4,10 @@
 
 
 <div class="jumbotron" style="margin: auto;width: 60%;border: 3px solid #73AD21;padding: 10px;"  >
-  <h1 class="display-4" style="text-decoration: underline;">{{$post->title}}</h1>
-  <img src="{{ filter_var($post->image, FILTER_VALIDATE_URL) ? $post->image :Voyager::image($post->image)}}" style ="width: 100%">
+  <h2 class="display-4" style="display: inline;margin-left: 25%">{{$post->title}}</h2><br><br>
+  <img src="{{ filter_var($post->image, FILTER_VALIDATE_URL) ? $post->image :Voyager::image($post->image)}}" style =" display: block;margin-left: auto;margin-right: auto; max-width: 100%;
+  height: 500px;"><br>
+  <h2>Description : </h2>
   <p class="lead" style="margin-top: 10px;">{!!$post->body!!}</p>
   <hr class="my-4">
   <p>{{$post->slug}}</p>
